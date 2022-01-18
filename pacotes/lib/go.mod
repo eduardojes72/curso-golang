ac063591691e1d897ce4a3f0ec8c214cf33ace15
Eduardo <eduardojes72@gmail.com>
2022-01-18 10:44:21 -0300

    pacotes

---
+++ pacotes/lib/go.mod
@@ -0,0 +1,5 @@
+module lib
+
+go 1.17
+
+require github.com/eduardojes72/goarea v0.0.0-20220117205602-462aac35ad04 // indirect
---
+++ pacotes/lib/lib.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eduardojes72/goarea"
+)
+
+func main() {
+	fmt.Println(goarea.Circ(4.0))
+}
